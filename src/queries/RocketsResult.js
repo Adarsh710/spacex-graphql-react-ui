3c5dc343426869470cd53b3633f00e159ea57c67
Adarsh710 <adarshagrawal777@gmail>
2021-01-09 09:56:39 +0530

    Added query for rockets Added another media query to handle hero image height Fixed: warning for same key

--- src/queries/RocketsResult.js
+++ src/queries/RocketsResult.js
@@ -1,12 +1,60 @@
 import React from 'react'
+import { useQuery, gql } from "@apollo/client";
+import RocketLoader from "../loaders/RocketLoader";
+
+const RocketsResultQuery = gql`
+{
+  rocketsResult {
+    result {
+      totalCount
+    }
+    data {
+      active
+      company
+      cost_per_launch
+      description
+      id
+      height {
+        meters
+      }
+      landing_legs {
+        material
+        number
+      }
+      mass {
+        kg
+      }
+      name
+      success_rate_pct
+      type
+      engines {
+        number
+        type
+        layout
+        version
+      }
+      first_flight
+      stages
+      diameter {
+        meters
+      }
+      boosters
+    }
+  }
+}
+`
 
 export default function RocketsResult(props) {
+  const { loading, error, data } = useQuery(RocketsResultQuery);
   const { value, index } = props;
+
+  if (loading) return <RocketLoader />;
+  if (error) return <p>Error :(</p>;
 
   return (
     <>
       {value === index && (<div className="rockets-container">
-        Coming Soon...
+        {JSON.stringify(data.rocketsResult)}
       </div>)}
     </>
   );
