1af11707bfc6cc8c5c556fde972bf571c6bafb85
Adarsh710 <adarshagrawal777@gmail>
2021-01-17 20:57:56 +0530

    Updated rocket card ui and styling

--- src/components/RocketCard/RocketCard.js
+++ src/components/RocketCard/RocketCard.js
@@ -1,35 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Card, CardActionArea, CardContent, Typography, Grid } from '@material-ui/core';
+import { Typography, Divider } from '@material-ui/core';
 import './RocketCard.css'
 
 function RocketCard(props) {
   return (
-    <>
-      <Grid
-        className="grid-container"
-        container
-        direction="row"
-        alignItems="center"
-        justify="center"
-        spacing={3}>
-        {
-          props.rockets.data.map((rocket) => (
-            <Grid item sm={8} md={4} lg={3} xl={2} key={`${rocket.id + '-' + Math.random()}`} className="grid-item">
-              <Card className='card'>
-                <CardActionArea className="card-CTA">
-                  <CardContent>
-                    <Typography variant="h6" className="text-ellipsis" component="h3" title={rocket.name}>
-                      {rocket.name}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ))
-        }
-      </Grid>
-    </>
+    <ul className="rocket-list">
+      {
+        props.rockets.data.map((rocket) => (
+          <li key={`${rocket.id + '-' + Math.random()}`}>
+            <div>
+            <Typography variant="h5" className="text-ellipsis" component="h2" title={rocket.name}>
+              {rocket.name}
+            </Typography>
+            <Typography variant="body2" align="justify">
+              {rocket.description}
+            </Typography>
+            </div>
+            <Divider  key={rocket.id} variant="fullWidth" />
+          </li>
+        ))
+      }
+    </ul>
   )
 }
 
